f65f686b7c58a7f1ae0c3d50cc3baa6499683e24
Angel <a_martinez00@outlook.com>
2023-04-06 15:31:15 -0400

    added app.tsx changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { SigninPage } from './pages/SigninPage';
+import { NewReimbursement } from './pages/NewReimbursement';
+import { ManageReimbursementPage } from './pages/ManageReimbursementPage';
 
 function App() {
   return (
-    <h1>Revature ERS</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<SigninPage/>}/>
+      <Route path='/home' element={<HomePage/>}/>
+      <Route path='/newreimbursement' element={<NewReimbursement/>}/>
+      <Route path='/managereimbursements' element={<ManageReimbursementPage/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
