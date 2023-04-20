8ce3949e2d5ddad91fb475f6aee22041f1244f5d
Angel <a_martinez00@outlook.com>
2023-04-20 18:34:15 -0400

    updated url path

--- src/api/app-client.ts
+++ src/api/app-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const url = "http://3.133.89.242:3000"
+export const url = "http://3.134.85.143:3000"
 
 export const appClient = axios.create({
     baseURL: `${url}`,
